5557b509de6b8f520e7c98d7849d7daaa3b676da
kimiagastya <kimiagastya@gmail.com>
2024-04-27 13:46:22 +0700

    Front end login, logout, register, conditionals navbar done

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from main.views import show_landing_page
+from main.views import show_landing_page, register, login_user, logout_user, show_welcome_page
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_landing_page, name='show_landing_page'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login_user'),
+    path('logout/', logout_user, name='logout_user'),
+    path('welcome/', show_welcome_page, name='show_welcome_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,40 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
 
 def show_landing_page(request):
     return render(request, "landing-page.html")
+
+def show_welcome_page(request):
+    return render(request, "welcome.html")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login_user')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_landing_page')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:show_landing_page')
