e3e41dfabde0c06e5cbf9d9c40b45304a1710a8d
febrian-irv <123155363+febrian-irv@users.noreply.github.com>
2024-05-02 22:24:57 +0700

    resolving project apps conflict

--- pacilflix/urls.py
+++ pacilflix/urls.py
@@ -20,6 +20,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('trailers/', include('trailer.urls')),
+    path('watch/', include('tayangan.urls')),
+    path('reviews/', include('ulasan.urls')),
     path('daftar_kontributor', include('daftar_kontributor.urls')),
     path('langganan', include('langganan.urls')),
 ]
