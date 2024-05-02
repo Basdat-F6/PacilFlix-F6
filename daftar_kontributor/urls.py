b613855c3c1d0084e97911a63bc9ab53e39ff71f
febrian-irv <123155363+febrian-irv@users.noreply.github.com>
2024-05-02 20:57:13 +0700

    Front End daftar_kontributor

---
+++ daftar_kontributor/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from daftar_kontributor.views import show_daftar_kontributor
+
+app_name = 'daftar_kontributor'
+
+urlpatterns = [
+    path('/', show_daftar_kontributor, name='show_daftar_kontributor'),
+]
--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'daftar_kontributor',
 ]
 
 MIDDLEWARE = [
