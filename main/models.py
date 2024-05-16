b15d6254394e3467ab7c0a113b59021b8861d765
kimiagastya <kimiagastya@gmail.com>
2024-05-17 02:37:57 +0700

    Authentication done

--- main/models.py
+++ main/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    negara_asal = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.user.username
