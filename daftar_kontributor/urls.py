24651d62dba8932dc9c60850c41e24f93bc134e2
febrian-irv <123155363+febrian-irv@users.noreply.github.com>
2024-05-18 06:13:28 +0700

    refactoring daftar_kontributor

--- daftar_kontributor/urls.py
+++ daftar_kontributor/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from daftar_kontributor.views import show_daftar_kontributor, show_daftar_kontributor_pemain, show_daftar_kontributor_penulis_skenario, show_daftar_kontributor_sutradara
+from daftar_kontributor.views import show_daftar_kontributor
 
 app_name = 'daftar_kontributor'
 
 urlpatterns = [
-    path('', show_daftar_kontributor, name='show_daftar_kontributor'),
-    path('pemain', show_daftar_kontributor_pemain, name='show_daftar_kontributor_pemain'),
-    path('penulis_skenario', show_daftar_kontributor_penulis_skenario, name='show_daftar_kontributor_penulis_skenario'),
-    path('sutradara', show_daftar_kontributor_sutradara, name='show_daftar_kontributor_sutradara'),
+    path('<str:filter>', show_daftar_kontributor, name='show_daftar_kontributor'),
 ]
--- daftar_kontributor/views.py
+++ daftar_kontributor/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from utils.query import *
 from django.contrib import messages 
 
-def show_daftar_kontributor(request):
+def show_daftar_kontributor(request, filter):
     username_cookie = request.COOKIES.get('username')
     connection, cursor = get_db_connection()
     if connection is None or cursor is None:
         messages.error(request, 'Database connection failed')
     else:
         try:
-            cursor.execute('''
+            query_semua = '''
                 SELECT
                     c."id",
                     c."nama",
@@ -36,7 +36,47 @@ def show_daftar_kontributor(request):
                 ) roles ON c."id" = roles."id"
                 GROUP BY
                     c."id";
-            ''')
+            '''
+
+            query_filter = '''
+                SELECT
+                    c."id",
+                    c."nama",
+                    STRING_AGG(DISTINCT roles.role, ', ') AS roles,
+                    c."jenis_kelamin",
+                    c."kewarganegaraan"
+                FROM
+                    "CONTRIBUTORS" c
+                LEFT JOIN (
+                    SELECT
+                        p."id",
+                        'PEMAIN' AS role
+                    FROM "PEMAIN" p
+                    UNION ALL
+                    SELECT
+                        s."id",
+                        'SUTRADARA' AS role
+                    FROM "SUTRADARA" s
+                    UNION ALL
+                    SELECT
+                        ps."id",
+                        'PENULIS SKENARIO' AS role
+                    FROM "PENULIS_SKENARIO" ps
+                ) roles ON c."id" = roles."id"
+                WHERE roles.role LIKE %s
+                GROUP BY
+                    c."id";
+            '''
+
+            if (filter == 'semua'):
+                cursor.execute(query_semua)
+            elif(filter == 'pemain'):
+                cursor.execute(query_filter, ['PEMAIN'])
+            elif(filter == 'sutradara'):
+                cursor.execute(query_filter, ['SUTRADARA'])
+            elif(filter == 'penulis_skenario'):
+                cursor.execute(query_filter, ['PENULIS SKENARIO'])
+
             data_kontributor = cursor.fetchall()
             context = {
                 'username_cookie': username_cookie,
@@ -44,156 +84,6 @@ def show_daftar_kontributor(request):
             }
             connection.commit()
             return render(request, "daftar-kontributor.html", context)
-        except Exception as error:
-            connection.rollback()
-            error_message = str(error).split('CONTEXT')[0]
-            messages.error(request, error_message)
-        finally:
-            cursor.close()
-            connection.close()
-
-def show_daftar_kontributor_pemain(request):
-    username_cookie = request.COOKIES.get('username')
-    connection, cursor = get_db_connection()
-    if connection is None or cursor is None:
-        messages.error(request, 'Database connection failed')
-    else:
-        try:
-            cursor.execute('''
-                SELECT
-                    c."id",
-                    c."nama",
-                    STRING_AGG(DISTINCT role, ', ') AS roles,
-                    c."jenis_kelamin",
-                    c."kewarganegaraan"
-                FROM
-                    "CONTRIBUTORS" c
-                LEFT JOIN (
-                    SELECT
-                        p."id",
-                        'PEMAIN' AS role
-                    FROM "PEMAIN" p
-                    UNION ALL
-                    SELECT
-                        s."id",
-                        'SUTRADARA' AS role
-                    FROM "SUTRADARA" s
-                    UNION ALL
-                    SELECT
-                        ps."id",
-                        'PENULIS SKENARIO' AS role
-                    FROM "PENULIS_SKENARIO" ps
-                ) roles ON c."id" = roles."id"
-                GROUP BY
-                    c."id";
-            ''')
-            data_kontributor = cursor.fetchall()
-            context = {
-                'username_cookie': username_cookie,
-                'data_kontributor': data_kontributor
-            }
-            connection.commit()
-            return render(request, "pemain-only.html", context)
-        except Exception as error:
-            connection.rollback()
-            error_message = str(error).split('CONTEXT')[0]
-            messages.error(request, error_message)
-        finally:
-            cursor.close()
-            connection.close()
-
-def show_daftar_kontributor_sutradara(request):
-    username_cookie = request.COOKIES.get('username')
-    connection, cursor = get_db_connection()
-    if connection is None or cursor is None:
-        messages.error(request, 'Database connection failed')
-    else:
-        try:
-            cursor.execute('''
-                SELECT
-                    c."id",
-                    c."nama",
-                    STRING_AGG(DISTINCT role, ', ') AS roles,
-                    c."jenis_kelamin",
-                    c."kewarganegaraan"
-                FROM
-                    "CONTRIBUTORS" c
-                LEFT JOIN (
-                    SELECT
-                        p."id",
-                        'PEMAIN' AS role
-                    FROM "PEMAIN" p
-                    UNION ALL
-                    SELECT
-                        s."id",
-                        'SUTRADARA' AS role
-                    FROM "SUTRADARA" s
-                    UNION ALL
-                    SELECT
-                        ps."id",
-                        'PENULIS SKENARIO' AS role
-                    FROM "PENULIS_SKENARIO" ps
-                ) roles ON c."id" = roles."id"
-                GROUP BY
-                    c."id";
-            ''')
-            data_kontributor = cursor.fetchall()
-            context = {
-                'username_cookie': username_cookie,
-                'data_kontributor': data_kontributor
-            }
-            connection.commit()
-            return render(request, "sutradara-only.html", context)
-        except Exception as error:
-            connection.rollback()
-            error_message = str(error).split('CONTEXT')[0]
-            messages.error(request, error_message)
-        finally:
-            cursor.close()
-            connection.close()
-
-def show_daftar_kontributor_penulis_skenario(request):
-    username_cookie = request.COOKIES.get('username')
-    connection, cursor = get_db_connection()
-    if connection is None or cursor is None:
-        messages.error(request, 'Database connection failed')
-    else:
-        try:
-            cursor.execute('''
-                SELECT
-                    c."id",
-                    c."nama",
-                    STRING_AGG(DISTINCT role, ', ') AS roles,
-                    c."jenis_kelamin",
-                    c."kewarganegaraan"
-                FROM
-                    "CONTRIBUTORS" c
-                LEFT JOIN (
-                    SELECT
-                        p."id",
-                        'PEMAIN' AS role
-                    FROM "PEMAIN" p
-                    UNION ALL
-                    SELECT
-                        s."id",
-                        'SUTRADARA' AS role
-                    FROM "SUTRADARA" s
-                    UNION ALL
-                    SELECT
-                        ps."id",
-                        'PENULIS SKENARIO' AS role
-                    FROM "PENULIS_SKENARIO" ps
-                ) roles ON c."id" = roles."id"
-                GROUP BY
-                    c."id";
-            ''')
-            data_kontributor = cursor.fetchall()
-            context = {
-                'username_cookie': username_cookie,
-                'data_kontributor': data_kontributor
-            }
-            connection.commit()
-            return render(request, "penulis_skenario-only.html", context)
         except Exception as error:
             connection.rollback()
             error_message = str(error).split('CONTEXT')[0]
